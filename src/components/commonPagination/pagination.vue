5bcbc83691a55cf3fd3a7872489641b4e60cdcc5
guanshoujing@cncloudsec.com <17600343254@163.com>
2020-08-04 16:19:35 +0800

    封装公共的分页

---
+++ src/components/commonPagination/pagination.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <a-pagination
+      show-size-changer
+      :current="pagination.current"
+      :pageSize="pagination.pageSize"
+      :total="pagination.total"
+      @change="pageChange"
+      @showSizeChange="pageSizeChange"
+    />
+  </div>
+</template>
+
+<script>
+import { Pagination } from 'ant-design-vue'
+export default {
+  name: 'Pagination',
+  components: {
+    aPagination: Pagination
+  },
+  props: {
+    pagination: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    pagination: function (val) {
+      console.log(val)
+    }
+  },
+  methods: {
+    pageChange (page, pageSize) {
+      this.$emit('changePage', page, pageSize)
+    },
+    pageSizeChange (page, pageSize) {
+      this.$emit('changePageSize', page, pageSize)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
